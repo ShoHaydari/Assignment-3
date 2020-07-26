a1afee68cfc8059e9db3cb4e9d06478766b37cbf
ShoHaydari <68448470+ShoHaydari@users.noreply.github.com>
2020-07-26 18:24:27 -0400

    Update HourlyWorker.h

--- HourlyWorker.h
+++ HourlyWorker.h
@@ -1,1 +1,33 @@
 
+//Cédric Hakizimana 40112938
+//Shokhad Haydari 40111203
+//Assignment #3
+//Platform/IDE used: Microsoft Visual Studio
+#pragma once
+#include "Employee.hpp"
+
+#ifndef HourlyWorker_hpp
+#define HourlyWorker_hpp
+
+class HourlyWorker:public Employee {
+public:
+    HourlyWorker(const string&,const string&,const string&, double=0.0, double=0.0);
+    void setwage(double);
+    void sethours(double);
+    double getwage() const;
+    double gethours() const;
+    virtual double earnings()const override;
+    virtual string toString() const override;
+    
+private:
+    double wage; // determines the wage earned
+    double hours; //stores the number of pieces made
+
+
+
+
+
+};
+
+#endif /* HourlyWorker_hpp */
+
