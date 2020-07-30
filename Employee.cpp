86a0e46bdad88210ed90be1a1837d167ad6e752d
ShoHaydari <68448470+ShoHaydari@users.noreply.github.com>
2020-07-30 12:13:01 -0400

    Update Employee.cpp

--- Employee.cpp
+++ Employee.cpp
@@ -9,40 +9,52 @@
 
 using namespace std;
 
-Employee::Employee(const string& first, const string& last, const string& ssn){
+Employee::Employee(const string& first, const string& last, const string& ssn)
+//the data members are initialized through set functions
+{
+    setfirstname(first);
+    setlastname(last);
+    setsocialsecuritynumber(ssn);
+}
+
+void Employee::setfirstname(const string& first)
+//sets the firstname to the string "first" passed
+{
     firstname = first;
+}
+
+void Employee::setlastname(const string& last)
+//sets the lastname to the string "last" passed
+{
     lastname = last;
+}
+
+void Employee::setsocialsecuritynumber(const string& ssn)
+//sets the socialsecuritynumber to the string "ssn" passed
+{
     socialsecuritynumber = ssn;
-
 }
 
-void Employee::setfirstname(const string& first) {
-    firstname = first;
-
-}
-
-void Employee::setlastname(const string& last) {
-    lastname = last;
-
-}
-
-void Employee::setsocialsecuritynumber(const string& ssn) {
-    socialsecuritynumber = ssn;
-
-}
-
-string Employee::getfirstname() const{
+string Employee::getfirstname() const
+//prints the firstname
+{
     return firstname;
 }
 
-string Employee::getlastname() const{
+string Employee::getlastname() const
+//prints lastname
+{
     return lastname;
 }
 
-string Employee::getsocialsecuritynumber() const{
+string Employee::getsocialsecuritynumber() const
+//prints socialsecuritynumber
+{
     return socialsecuritynumber;
 }
 
-string Employee::toString() const{
+string Employee::toString() const
+//prints all three data members assigned by the set functions
+{
     return getfirstname() + " " + getlastname() + "\nSocial Security Number: " + getsocialsecuritynumber();
 }
