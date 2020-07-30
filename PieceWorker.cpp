f07e8e7571dd940acea705255e00a28814c74fa1
ShoHaydari <68448470+ShoHaydari@users.noreply.github.com>
2020-07-30 12:10:59 -0400

    Update PieceWorker.cpp

--- PieceWorker.cpp
+++ PieceWorker.cpp
@@ -6,34 +6,49 @@
 #include <iostream>
 #include <sstream>
 #include "PieceWorker.hpp"
+using namespace std;
 
-PieceWorker::PieceWorker(const string& first,const string& last,const string& ssn, double w, double p): Employee(first,last,ssn) {
+PieceWorker::PieceWorker(const string& first,const string& last,const string& ssn, double w, double p): Employee(first,last,ssn)
+//constructor that initializes the wage and pieces sold through the setfunctions, the other variables are initialized by Employees constructor
+{
     setwage(w);
     setpieces(p);
 }
 
-void PieceWorker::setwage(double w) {
+void PieceWorker::setwage(double w)
+//sets wage
+{
     wage = w;
 }
 
-void PieceWorker::setpieces(double p)  {
+void PieceWorker::setpieces(double p)
+//sets the number of pieces sold
+{
     pieces = p;
 }
 
-double PieceWorker::getwage() const{
+double PieceWorker::getwage() const
+//returns the wage per pieces sold
+{
     return wage;
 }
 
-double PieceWorker::getpieces()const {
+double PieceWorker::getpieces()const
+//returns the pieces sold
+{
     return pieces;
 }
 
-double PieceWorker::earnings() const{
+double PieceWorker::earnings() const
+//returns the amount earned per pieces sold
+{
     cout<<"Earned $: ";
-    return getwage() * getpieces();
+    return wage*pieces;
 }
 
-string PieceWorker::toString() const{
+string PieceWorker::toString() const
+//Adds feature to the toString function of employee class. In this case, it returns the number of pieces sold and the wage per pieces sold. Note that the firstname, lastname and socialsecuritynumber is printed by the toString function of Employee
+{
     ostringstream output;
     output <<"Piece Worker: "<<Employee::toString()<<"\nPieces sold: "<<getpieces()<<"\nWage per pieces sold: "<<getwage();
     return output.str();
