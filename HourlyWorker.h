5234f5422a6bdeed5aa6ff06fe2867b8ea827f02
ShoHaydari <68448470+ShoHaydari@users.noreply.github.com>
2020-07-30 12:12:15 -0400

    Update HourlyWorker.h

--- HourlyWorker.h
+++ HourlyWorker.h
@@ -9,25 +9,21 @@
 #ifndef HourlyWorker_hpp
 #define HourlyWorker_hpp
 
-class HourlyWorker:public Employee {
+class HourlyWorker:public Employee //inherits the member functions of Employee
+{
 public:
-    HourlyWorker(const string&,const string&,const string&, double=0.0, double=0.0);
-    void setwage(double);
-    void sethours(double);
-    double getwage() const;
-    double gethours() const;
-    virtual double earnings()const override;
-    virtual string toString() const override;
+    HourlyWorker(const std::string&,const std::string&,const std::string&, double=0.0, double=0.0); //constructor
+    void setwage(double); //sets the wage per hour
+    void sethours(double); //sets the hours worked
+    double getwage() const; // returns the wage
+    double gethours() const; //returns the hours worked
+    virtual double earnings()const override; //calculate the earnings per week
+    virtual std::string toString() const override; //override of the toString function from Employee class
     
 private:
     double wage; // determines the wage earned
     double hours; //stores the number of pieces made
 
-
-
-
-
 };
 
 #endif /* HourlyWorker_hpp */
-
